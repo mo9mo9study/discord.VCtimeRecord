923e0cbb42a432185eaaa82651d0f0d66c827a5b
su-pleiades <su.impreza.itengineer@gmail.com>
2021-01-20 02:10:58 +0900

    [#24]日付を跨ぐ処理追加、[#26]Cog化完了
    ステージ環境で動作済み、特に日跨ぎでエラーが出ないか確認
    Cogに対応するようにコード修正
    退出時、Discordメッセージは１件・裏の記録は２件になるよう修正
    各所コメント追記

--- Cogs/Studyrecord/entryExit.py
+++ Cogs/Studyrecord/entryExit.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 from discord.ext import commands, tasks
 import discord
@@ -16,19 +16,25 @@ class ENTRY_EXIT(commands.Cog):
         self.logsPath = "/home/centos/repos/discord.VCtimeRecord/entry_exit/timelog/"
         self.NotRecordChannels = "時間記録無"
 
-    async def writeLog(time,name,m,mdta=''):
+    # ディレクトリの作成
+    # ユーザーの勉強記録ログのファイルを作成
+    async def writeLog(self, time,name,m,mdta=''):
         print(f'{name} : <ログの書き込み>')
         if not os.path.isdir(self.logsPath):
             os.mkdir(self.logsPath)
+            print("dir create")
         filePath = f'{self.logsPath}{name}'
         if os.path.isfile(filePath):
+            print("userfile create")
             with open(filePath, "a", encoding="utf-8") as f:
                 f.write(str(time) + ',' + name + ',' + m + ',' + mdta + '\n')
         else:
+       	    print("userfile add")
             with open(filePath, "w", encoding="utf-8") as f:
                 f.write(str(time) + ',' + name + ',' + m + ',' + mdta + '\n')
 
-    def splitTime(name):
+    # ログファイルから入室時間を取得
+    def splitTime(self, name):
         print(f'{name} : <ログの読み込み>')
         filePath = f'{self.logsPath}{name}'
         with open(filePath) as f:
@@ -40,8 +46,7 @@ class ENTRY_EXIT(commands.Cog):
             return dtResult
 
     @commands.Cog.listener()
-    async def on_voice_state_update(member, before, after):
-        # 原因がわからず、入室時に二度処理が走っているため。入室後に走るイベントをretunする処理追加
+    async def on_voice_state_update(self, member, before, after):
         if before.channel == after.channel:
             return
         print(f"[{datetime.now()}]{member.name} : ステータスが変更されました (before){before.channel}/(after){after.channel}")
@@ -50,17 +55,18 @@ class ENTRY_EXIT(commands.Cog):
             return
         if member.guild.id == self.guild_id and (before.channel != after.channel):
             now = datetime.utcnow() + timedelta(hours=9)
-            alert_channel = client.get_channel(self.channel_id)
+            record_channel = self.bot.get_channel(self.channel_id)
             # VC入室時 or 勉強記録なし--->勉強記録あり
             if before.channel is None or self.NotRecordChannels in before.channel.name and before.channel != after.channel:
                 print(f'{member.name} : 入室時の処理')
                 self.pretime_dict['beforetime'] = datetime.now()
                 msg = f'{now:%m/%d %H:%M} 　 {member.name}   joined the  {after.channel.name}'
-                #writeLog(datetime.now(),member.name,msg)
+                await self.writeLog(datetime.now(),member.name,msg)
                 if  self.NotRecordChannels in after.channel.name:
                     print(f'{member.name} : self.NotRecordChannels リストに記載されているので記録しません')
                     return
-                #await alert_channel.send(msg)
+                # Discordにメッセージを出力
+                await record_channel.send(msg)
             # VC退室時 or 勉強記録あり--->勉強記録なし
             elif after.channel is None or self.NotRecordChannels in after.channel.name and before.channel != after.channel:
                 print(f'{member.name} : 退室時の処理')
@@ -68,17 +74,17 @@ class ENTRY_EXIT(commands.Cog):
                     print(f'{member.name} : self.NotRecordChannels リストに記載されているので記録しません')
                     return
                 msg = f'[{now:%m/%d %H:%M} ]  {member.name}  joined  {before.channel.name} '
-                dtBeforetime = splitTime(member.name)
+                dtBeforetime = self.splitTime(member.name)
+                print(f"dtDefore: {dtBeforetime}")
                 try:
                     duration_time = dtBeforetime - datetime.now()
                     duration_time_adjust = int(duration_time.total_seconds()) * -1
                     print(f"dutation_time_adjust: {duration_time_adjust}")
-                    if duration_time_adjust >= 60:
+                    if duration_time_adjust >= 60: # 1分以上は記録
                         print(f'{member.name} : 60sec以上')
                         minute_duration_time_adjust = int(duration_time_adjust) // 60
                         msg = "-->[" + member.name + "]   Study time： " + str(minute_duration_time_adjust) + "/分"
-                        #writeLog(datetime.now(),member.name,msg,str(minute_duration_time_adjust)) #不要になる予定
-                        if duration_time_adjust >= 300:
+                        if duration_time_adjust >= 300: # 5分以上は記録＆Discordに出力
                             entry_date = date(dtBeforetime.year, dtBeforetime.month, dtBeforetime.day)
                             print(f"entry_date: {entry_date}")
                             exit_date = date.today()
@@ -91,12 +97,15 @@ class ENTRY_EXIT(commands.Cog):
                                 day_studytime = int((datetime.combine(date.today(), time(0,0)) - datetime.now()).total_seconds()) * -1 // 60
                                 result_studytimes.append(day_studytime) 
                                 print(f"list:{result_studytimes}")
-                                for result_time in result_studytimes:
-                                    #writeLog(datetime.now(),member.name,msg,str(result_time))
-                                    print(f"書き込みログ：{datetime.now()},{member.name},{msg},{str(result_time)}")
+                            else: # 入室と退室が同日の場合の処理
+                                result_studytimes = [minute_duration_time_adjust]
+                            for result_time in result_studytimes:
+                                await self.writeLog(datetime.now(),member.name,msg,str(result_time))
+                                print(f"書き込みログ：{datetime.now()},{member.name},{msg},{str(result_time)}")
 
                             print(f'{member.name} : 退室ログをDiscordに出力')
-                            #await alert_channel.send(msg)
+                            # Discordにメッセージを出力
+                            await record_channel.send(msg)
                 except KeyError:
                     print(f'{member.name} : except')
                     pass

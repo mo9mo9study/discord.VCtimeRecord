557f63c82dee6531af987190934a1138cc006886
su-pleiades <su.impreza.itengineer@gmail.com>
2021-04-03 15:58:54 +0900

    fix args error

--- Cogs/Studyrecord/entryExit.py
+++ Cogs/Studyrecord/entryExit.py
@@ -14,7 +14,6 @@ class ENTRY_EXIT(commands.Cog):
         self.channel_id = 673006702924136448  # times_supleiades
         self.pretime_dict = {}
         self.NotRecordChannels = "時間記録無"
-        self.now = f" {(datetime.utcnow() + timedelta(hours=9)):%m-%d %H:%M} "
 
     async def writeLog(self,
                        study_dt,
@@ -47,16 +46,16 @@ class ENTRY_EXIT(commands.Cog):
         return obj
 
     # メッセージを送信
-    async def sendstudytimelogmsg(self, member, channel, access,
+    async def sendstudytimelogmsg(self, now, member, channel, access,
                                   studytime=None):
         if self.NotRecordChannels in channel.name:
             return
-        print(f"[{self.now}] {member.name} {access}ログをDiscordに出力")
+        print(f"[{now}] {member.name} {access}ログをDiscordに出力")
         send_channel = self.bot.get_channel(self.channel_id)
         if access == "in":
-            msg = f"[{self.now}]  {member.name}  joined the  {channel.name}."
+            msg = f"[{now}]  {member.name}  joined the  {channel.name}."
         elif access == "out":
-            msg = f"[{self.now}]  {member.name}  Study time  {studytime} /分"
+            msg = f"[{now}]  {member.name}  Study time  {studytime} /分"
         await send_channel.send(msg)
 
     @commands.Cog.listener()
@@ -66,7 +65,8 @@ class ENTRY_EXIT(commands.Cog):
         if member.bot:
             print(f'{member.name} : BOTは記録しません')
             return
-        print(f"[{self.now}]{member.name} : {before.channel}/{after.channel}")
+        now = f" {(datetime.utcnow() + timedelta(hours=9)):%m-%d %H:%M} "
+        print(f"[{now}]{member.name} : {before.channel}/{after.channel}")
         study_dt = datetime.utcnow() + timedelta(hours=9)
     # VC入室時 or 勉強記録なし--->勉強記録あり
         if (before.channel is None
@@ -78,7 +78,7 @@ class ENTRY_EXIT(commands.Cog):
             self.pretime_dict['beforetime'] = datetime.now()
             # DBに入室記録を挿入、Discordにメッセージを出力
             await self.writeLog(study_dt, member, channel, access)
-            await self.sendstudytimelogmsg(member, channel, access)
+            await self.sendstudytimelogmsg(now, member, channel, access)
     # VC退室時 or 勉強記録あり--->勉強記録なし
         elif (after.channel is None
               or self.NotRecordChannels in after.channel.name
@@ -93,7 +93,7 @@ class ENTRY_EXIT(commands.Cog):
                 duration_time = dtBeforetime - datetime.now()
                 duration_time_adjust = int(duration_time.total_seconds()) * -1
                 print(f"dutation_time_adjust: {duration_time_adjust}")
-                # if duration_time_adjust >= 60:  # 1分以上は記録
+#                if duration_time_adjust >= 60:  # 1分以上は記録
                 if duration_time_adjust >= 1:  # 1分以上は記録
                     print(f'{member.name} : 60sec以上')
                     minute_duration_time_adjust = int(
@@ -142,7 +142,8 @@ class ENTRY_EXIT(commands.Cog):
                                                 str(result_time))
                     if duration_time_adjust >= 300:  # 5分以上は記録＆Discordに出力
                         studytime = minute_duration_time_adjust
-                        await self.sendstudytimelogmsg(member,
+                        await self.sendstudytimelogmsg(now,
+                                                       member,
                                                        channel,
                                                        access,
                                                        studytime=studytime)
